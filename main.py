989b98de9c8bab4f6421ba529eabd5815ecf9d90
tomasaccini <tomasaccini@gmail.com>
2018-04-12 12:57:49 -0300

    Added auto-save in every write action

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     3-Ver todos los errores sin resolver\n\
     4-Ver todos los errores sin resolver de un determinado TDA\n\
     5-Resolver un error existente\n\
-    6-Guardar y salir"
+    6-Salir"
     input_usuario = 0
     while(input_usuario != 6):
         print("\n\nBienvenido")
@@ -18,16 +18,17 @@ def main():
         except:
             print("Opción no válida. No es decimal.")
             continue
-        if(input_usuario == 1): sg.add_error_with_solution()
-        elif(input_usuario == 2): sg.add_error_without_solution()
+        if(input_usuario == 1):
+            sg.add_error_with_solution()
+            sg.finish()
+        elif(input_usuario == 2):
+            sg.add_error_without_solution()
+            sg.finish()
         elif(input_usuario == 3): sg.print_all_unsolved_errors()
         elif(input_usuario == 4): sg.print_all_unsolved_errors_from_tda()
-        elif(input_usuario == 5): sg.add_solution_to_error()
+        elif(input_usuario == 5):
+            sg.add_solution_to_error()
+            sg.finish()
         elif(input_usuario == 6): break
         else: print("Opción no válida. No es una de las opciones posibles.")
-        sg.finish()
-
-
-
-
 main()
