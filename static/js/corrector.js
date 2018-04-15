7349b6180d259786b9eb2c44b41da57bda65ed71
Flox <florrr1997@gmail.com>
2018-04-15 18:02:28 -0300

    needs refactoring, introduction to collapse

--- static/js/corrector.js
+++ static/js/corrector.js
@@ -68,13 +68,36 @@ function createCheckbox(error_id, error, tda){
 }
 
 function createSolution(error_id, error, solution){
-  var container = document.getElementById('containerSolutions');
-  var p = document.createElement('p');
-  p.style.display = "none";
-  p.id = error_id+"p";
-  p.innerHTML = "- <b>" + error + ":</b> " + solution;
-  container.appendChild(p);
+  var divContainer = document.getElementById('accordion');
+  var divPrincipal = document.createElement('div');
+  divPrincipal.className = 'card';
+  var divName = document.createElement('div');
+  divName.className = 'card-header';
+  divName.id = "heading" + error_id;
+  var h5 = document.createElement('h5');
+  h5.className='mb-0';
+  var button = document.createElement('button');
+  button.className = "btn btn-link";
+  button.setAttribute('data-toggle','collapse');
+  button.setAttribute('data-target','#' + 'collapse' + error_id);
+  button.setAttribute('aria-expanded',"true");
+  button.setAttribute('aria_controls','collapse' + error_id);
+  button.innerHTML = 'Solución';
+  h5.appendChild(button);
+  divName.appendChild(h5);
 
+  var divbox = document.createElement('div');
+  divbox.id = 'collapse' + error_id
+  divbox.className = "collapse show"
+  divbox.setAttribute('data-parent','#accordion');
+  divbox.setAttribute('aria-labelledby',"heading" + error_id);
+  var divText = document.createElement('card-body');
+  divText.innerHTML = solution;
+  divbox.appendChild(divText);
+
+  divPrincipal.appendChild(divName);
+  divPrincipal.appendChild(divbox);
+  divContainer.appendChild(divPrincipal);
 }
 
 function generateMail(){
